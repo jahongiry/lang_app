9faf46de883a816309bf68811d540de6072b339a
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-05-02 20:40:02 +0500

    Remove

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module LanguageApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.autoload_paths += %W(#{config.root}/app/services)
 
     # Configuration for the application, engines, and railties goes here.
     #
