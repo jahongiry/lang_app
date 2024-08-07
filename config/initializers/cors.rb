37e168b1500fd294302067acc49f527ad722601e
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-08-07 16:00:25 +0500

    Add cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://dashboard-steel-phi.vercel.app', 'https://sooyaa.uz', 'localhost:3001'
+    origins '*'
 
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
